42d932f97c0cb33a45b5e059ace0bed651d36a05
Stephane Nicoll <snicoll@pivotal.io>
2017-12-05 17:48:57 -0800

    Initial import

---
+++ src/main/java/com/example/banner/AnimatedBannerApplication.java
@@ -0,0 +1,12 @@
+package com.example.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimatedBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimatedBannerApplication.class, args);
+	}
+}
